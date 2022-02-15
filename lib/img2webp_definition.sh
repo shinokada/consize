325d13114f35b6ccf780f291a8a0f66c6eafe0bf
Shinichi Okada <okada.shin@gmail.com>
2022-02-15 17:25:00 +0900

    Added img2webp. This will convert PNG to WEBP, then JPEG/JPG to WEBP.

--- lib/img2webp_definition.sh
+++ lib/img2webp_definition.sh
@@ -1,7 +1,7 @@
-parser_definition_jpg2webp() {
+parser_definition_img2webp() {
     setup REST plus:true help:usage abbr:true -- \
         "Usage: ${2##*/} [options...] [arguments...]" ''
-    msg -- 'Convert JPG to PNG images and to WEBP format.' ''
+    msg -- 'Convert JPG/PNG images to WEBP format.' ''
     msg -- 'Options:'
     param DIR -d --dir -- "accepts --dir value / --dir=value"
     param QUALITY -q --quality -- "accepts --quality value / --quality=value"
---
+++ src/img2webp.sh
@@ -0,0 +1,26 @@
+fn_img2webp(){
+    if [[ -z "${DIR}" ]]; then
+        echo "Please specify an input directory."
+        exit 1
+    elif [[ -z "${QUALITY}" ]]; then
+        echo "Please specify image quality."
+        exit 1
+    fi
+
+    # for each png in the input directory convert to webp
+    for img in $( find ${DIR} -type f -iname "*.png" );
+    do
+        cwebp $img -q ${QUALITY} -o ${img%.*}.webp
+    done
+
+    # for each webp in the input directory convet to webp
+    for img in $( find ${DIR} -type f -iname "*.jpg" -o -iname "*.jpeg" );
+    do
+        # convert to png first
+        convert "$img" "${img%.*}".png
+
+        # then convert png to webp
+        cwebp "${img%.*}".png -q "${QUALITY}" -o "${img%.*}".webp
+    done
+
+}
