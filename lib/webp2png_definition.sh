4b743718bdba46443df0bb661c1c16130e5024a3
Shinichi Okada <okada.shin@gmail.com>
2022-02-16 20:14:09 +0900

    webp2jpg and webp2png definitions are added.

---
+++ lib/webp2png_definition.sh
@@ -0,0 +1,9 @@
+parser_definition_webp2png() {
+    setup REST plus:true help:usage abbr:true -- \
+        "Usage: ${2##*/} [options...] [arguments...]" ''
+    msg -- 'Convert JPG to PNG images and to WEBP format.' ''
+    msg -- 'Options:'
+    param DIR -d --dir -- "accepts --dir value / --dir=value"
+    param QUALITY -q --quality -- "accepts --quality value / --quality=value"
+    disp :usage -h --help
+}
