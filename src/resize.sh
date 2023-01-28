70c6382caa05db9c3114dc4de64b5cef2871b4b5
Shinichi Okada <okada.shin@gmail.com>
2023-01-29 06:56:35 +0900

    update: resize

--- src/resize.sh
+++ src/resize.sh
@@ -17,7 +17,8 @@ fn_resize(){
     mkdir "${INPUTDIR}/resized"
 
     # for each webp in the input directory
-    for img in $( find $(pwd) -type f -iname "*.webp" );
+    bannerColor 'Resizing webp files ...' "green" "*"
+    for img in $( find . -type f -iname "*.webp" );
     do
         # resize first
         convert "${img}" -resize "${DIMENSION}" "/resized/${img}"
