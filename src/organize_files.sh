77da5cb33fb4c98831235b04d05c5924a495a691
Shinichi Okada <okada.shin@gmail.com>
2023-02-09 11:49:32 +0900

    fix: add echo to organize_files

--- src/organize_files.sh
+++ src/organize_files.sh
@@ -6,10 +6,13 @@ fn_organize_files(){
         if [[ ${img:0:1} == '.' ]]; then
             continue
         fi
-        
+
         # get the type of the image
         img_type=$(basename `file --mime-type -b $img`)
 
+        echo "Processing file: $img"
+        echo "Image type: $img_type"
+        
         # create a directory for the image type
         mkdir -p $img_type
 
