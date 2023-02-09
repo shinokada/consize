ca08832d4abc4f06b6a9050eee3e7d6b7a4df519
Shinichi Okada <okada.shin@gmail.com>
2023-02-09 11:52:16 +0900

    fix: change if condition to skip dot files

--- src/organize_files.sh
+++ src/organize_files.sh
@@ -3,16 +3,13 @@ fn_organize_files(){
     for img in $( find ${INPUTDIR} -type f -iname "*" );
     do
         # Skip processing of dot files
-        if [[ ${img:0:1} == '.' ]]; then
+        if [[ $(basename "$img") =~ ^\.[^.] ]]; then
             continue
         fi
 
         # get the type of the image
         img_type=$(basename `file --mime-type -b $img`)
 
-        echo "Processing file: $img"
-        echo "Image type: $img_type"
-        
         # create a directory for the image type
         mkdir -p $img_type
 
