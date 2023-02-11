f19137d9d971bcba3f3e2157ec2a84560ef5dfbf
Shinichi Okada <okada.shin@gmail.com>
2023-02-12 05:37:03 +0900

    fix: update jpg2png

--- src/jpg2png.sh
+++ src/jpg2png.sh
@@ -1,20 +1,23 @@
 fn_jpg2png(){
     check_cmd convert
     
-    if [ ${OUTPUTDIR} ];then
+    if [ -n ${OUTPUTDIR} ];then
         mkdir -p ${OUTPUTDIR} 
     fi
     # for each jpg or jpeg in the input directory
     # this is not intentionally recursive
     for img in $( find ${INPUTDIR} -type f -iname "*.jpg" -o -iname "*.jpeg" );
     do
-        convert "$img" "${img%.*}".png
+        if [ -n ${OUTPUTDIR} ];then
+            convert "$img" "${OUTPUTDIR}/${img%.*}".png 
+        else
+            convert "$img" "${img%.*}".png 
 
-        if [ ${OUTPUTDIR} ];then
-        bannerColor "Moving converted files to ${OUTPUTDIR} ... " "blue" "*"
-        mv "${img%.*}".png "${OUTPUTDIR}"
-        bannerColor "Done." "green" "*"
-    fi
+        # if [ -n ${OUTPUTDIR} ];then
+        # bannerColor "Moving converted files to ${OUTPUTDIR} ... " "blue" "*"
+        # mkdir -p ${OUTPUTDIR} && mv "${img%.*}".png "${OUTPUTDIR}"
+        # bannerColor "Moved all the PNG files to ${OUTPUTDIR}." "green" "*"
+        fi
     done
 
     bannerColor 'Completed converting from JPG to PNG.' "green" "*"
